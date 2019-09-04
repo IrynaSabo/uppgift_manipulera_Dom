b0fc7f085ac5b726e7ec1d07e5acc562b5b08e6e
Förnamn Efternamn <dinepost@example.com>
2019-09-04 18:17:45 +0200

    added array

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ p.appendChild(textNode);
 
 let element=div.appendChild(p);//sätter ihopp div med p
 document.body.appendChild(element);// skriver ut på sidan
+
+let div2=document.createElement("div");
+div2.setAttribute("id","div2");
+let skills=["backa","cykla","rita","mate"];//skapar array
+div2.insertAdjacentHTML("beforeEnd", skills);
+document.body.appendChild(div2);
